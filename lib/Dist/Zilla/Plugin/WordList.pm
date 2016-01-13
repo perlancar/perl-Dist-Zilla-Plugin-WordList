1c962a6ce27e39ec1c457ff12eab1cea8089fdcf
perlancar (@pc-home) <perlancar@gmail.com>
2016-01-13 09:48:31 +0700

    Initial commit

---
+++ lib/Dist/Zilla/Plugin/WordList.pm
@@ -0,0 +1,105 @@
+package Dist::Zilla::Plugin::WordList;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Moose;
+use namespace::autoclean;
+
+use Data::Dmp;
+use Encode qw(is_utf8);
+
+with (
+    'Dist::Zilla::Role::FileMunger',
+    'Dist::Zilla::Role::FileFinderUser' => {
+        default_finders => [':InstallModules'],
+    },
+);
+
+sub munge_files {
+    no strict 'refs';
+    my $self = shift;
+
+    local @INC = ("lib", @INC);
+
+    my %seen_mods;
+    for my $file (@{ $self->found_files }) {
+        next unless $file->name =~ m!\Alib/((WordList/.+)\.pm)\z!;
+
+        my $package_pm = $1;
+        my $package = $2; $package =~ s!/!::!g;
+
+        # Add statistics to %STATS variable
+        {
+            require $package_pm;
+            my $wl = $package->new;
+
+            my $total_len = 0;
+            my %stats = (
+                num_words => 0,
+                num_words_contains_unicode => 0,
+                num_words_contains_whitespace => 0,
+                num_words_contains_nonword_chars => 0,
+                shortest_word_len => undef,
+                longest_word_len => undef,
+            );
+            $wl->each_word(
+                sub {
+                    my $word = shift;
+                    $stats{num_words}++;
+                    $stats{num_words_contains_unicode}++ if is_utf8($word);
+                    $stats{num_words_contains_whitespace}++ if $word =~ /\S/;
+                    $stats{num_words_contains_nonword_chars}++ if $word =~ /\W/;
+                    my $len = length($word);
+                    $stats{shortest_word_len} = $len
+                        if !defined($stats{shortest_word_len}) ||
+                        $len < $stats{shortest_word_len};
+                    $stats{longest_word_len} = $len if
+                        if !defined($stats{longest_word_len}) ||
+                        $len > $stats{longest_word_len};
+                });
+            $content =~ s{^(#\s*STATS)$}{"our %STATS = ".dmp(%stats)."; " . $1}em
+                or die "Can't replace #STATS for " . $file->name;
+            $self->log(["replacing #STATS for %s (%s)",
+                        $file->name, $abstract]);
+
+            $file->content($content);
+        }
+    } # foreach file
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+# ABSTRACT: Plugin to use when building WordList::* distribution
+
+=for Pod::Coverage .+
+
+=head1 SYNOPSIS
+
+In F<dist.ini>:
+
+ [Bencher::Scenario]
+
+
+=head1 DESCRIPTION
+
+This plugin is to be used when building C<WordList::*> distribution. Currently
+it does the following:
+
+=over
+
+=item * Replace C<# STATS> placeholder (which must exist) with word list statistics
+
+=back
+
+
+=head1 SEE ALSO
+
+L<WordList>
+
+L<Pod::Weaver::Plugin::WordList>
